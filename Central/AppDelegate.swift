37e3e026c253e6551771da47519270ecd3607db9
-- C4 -- <info@c4ios.com>
2015-07-08 23:19:00 -0700

    Initial Commit

---
+++ Central/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Central
+//
+//  Created by travis on 2015-07-08.
+//  Copyright (c) 2015 C4. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Central/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Central
+//
+//  Created by travis on 2015-07-08.
+//  Copyright (c) 2015 C4. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
